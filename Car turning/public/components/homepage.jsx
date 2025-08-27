8bb2a71862ac27b8c35a878e21f8c9cb283b8e99
Rottecraft <zbc24sdav@zbc.dk>
2025-08-27 12:01:34 +0200

    beginning

---
+++ Car turning/public/components/homepage.jsx
@@ -0,0 +1,38 @@
+
+export default function Homepage(){
+    return(
+        <>
+            <head>
+
+                <title>Mads' chiptuning</title>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"></link>
+            </head>
+            <div>
+                <div className="company-name-container">
+                    <div className="company-name">Mads' chiptuning</div>
+                    <div className="company-logo">insert logo here</div>
+                </div>
+                <nav className="navigation">
+                    <button>Kontakt</button>
+                    <button>Find din bil</button>
+                    <button>Tilbud</button>
+                    <button>Galleri</button>
+                </nav>
+            </div>
+            <body>
+                <section className="hero-section">
+                     <h1>UNLOCK BILENS FULDE POTENTIALE</h1>
+                     <h2> Optimering af benzin- og dieselmotorer</h2>
+                     <button>Kontakt</button>
+                     <button>Find din bil</button>
+                     <button>Tilbud</button>
+                </section>
+               
+                
+
+            </body>
+
+        </>
+
+    )
+}
